301e60259c226c5d6966916a6df094b655167209
pem <mckilpar000@hotmail.com>
2026-07-13 08:21:32 -0700

    setting up context provider for auth in case of page refresh

---
+++ frontend/src/assets/context/AuthContext.js
@@ -0,0 +1,8 @@
+import { createContext, useContext, useState, useEffect } from "react";
+
+const AuthContext = createContext(null);
+
+export function AuthProvider({ children }) {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+}
