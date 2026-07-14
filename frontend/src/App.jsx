518350cae3725b63bc17fe44ea3f8eba04a9347f
Najib Hazel Mosquera <najib.mosquera@hotmail.com>
2026-07-14 19:26:17 -0400

    Add main ride feed page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import useAuth from "./context/useAuth";
 import LoginForm from "./components/LoginForm";
 import RegisterForm from "./components/RegisterForm";
 import RideForm from "./components/RideForm";
+import RideFeedPage from "./components/RideFeedPage";
 import NavBar from "./components/NavBar";
 
 function GuestOnly({ children }) {
@@ -34,7 +35,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<GuestOnly><LoginForm /></GuestOnly>} />
         <Route path="/register" element={<GuestOnly><RegisterForm /></GuestOnly>} />
-        <Route path="/" element={<RequireAuth><div>Welcome!</div></RequireAuth>} />
+        <Route path="/" element={<RequireAuth><RideFeedPage /></RequireAuth>} />
         <Route path="/rides/new" element={<RequireAuth><RideForm mode="create" /></RequireAuth>} />
         <Route path="/rides/:postId/edit" element={<RequireAuth><RideForm mode="edit" /></RequireAuth>} />
       </Routes>
