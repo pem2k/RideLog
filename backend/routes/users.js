8122440a1d48c70691002b5da7165f2a2a551c32
pem <mckilpar000@hotmail.com>
2026-07-13 22:04:02 -0700

    added new patch route to allow user ot update bio and display name

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,11 +1,23 @@
 import { Router } from "express";
 import { ensureAuthenticated } from "../middleware/ensureAuthenticated.js";
-import { findById } from "../models/User.js";
+import { findById, updateUser } from "../models/User.js";
 import { ObjectId } from "mongodb";
 
 const router = Router();
 
 router.use(ensureAuthenticated);
+
+router.patch("/me", async (req, res, next) => {
+  try {
+    const updated = await updateUser(req.user._id, req.body);
+    res.status(200).json(updated);
+  } catch (err) {
+    if (err && err.status === 400) {
+      return res.status(400).json({ errors: err.errors });
+    }
+    next(err);
+  }
+});
 
 router.get("/:userId", async (req, res, next) => {
   try {
